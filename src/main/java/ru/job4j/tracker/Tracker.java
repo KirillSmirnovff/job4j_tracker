4dd4057b20a4627e813e94086bc30d5a133c5e0d
KirillSmirnovff <smirnovk.1990@gmail.com>
2021-10-13 13:44:36 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -54,4 +54,15 @@ public class Tracker {
         }
         return result;
     }
+
+    public boolean delete(int id) {
+        boolean result = false;
+        int index = indexOf(id);
+        if (index != -1) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size-- - 1] = null;
+            result = true;
+        }
+        return  result;
+    }
 }
