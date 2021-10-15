d6c2092b0e2a5d9f148e1025e26fba32b03f46a0
KirillSmirnovff <smirnovk.1990@gmail.com>
2021-10-15 13:31:30 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,20 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void ride() {
+        System.out.println("Автобус едет");
+    }
+
+    @Override
+    public void passengers(int count) {
+        System.out.println("В автобусе " + count + " пассажиров");
+    }
+
+    @Override
+    public double refuel(double fuel) {
+        double price = 2;
+        return price * fuel;
+    }
+}
