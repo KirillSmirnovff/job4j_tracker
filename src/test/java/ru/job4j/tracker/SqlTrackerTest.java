f95d3f1a4aea097bac05832628b3cd395f1eccf5
KirillSmirnovff <smirnovk.1990@gmail.com>
2022-12-24 17:12:23 +0600

    1. Что такое Mock-объект. [#504866]

--- src/test/java/ru/job4j/tracker/SqlTrackerTest.java
+++ src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -16,6 +16,9 @@ import java.util.Properties;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class SqlTrackerTest {
 
@@ -117,5 +120,68 @@ public class SqlTrackerTest {
         int id = bug.getId();
         tracker.delete(id);
         assertThat(tracker.findById(id), is(nullValue()));
+    }
+
+    @Test
+    public void whenReplaceMockito() {
+        Output out = new StubOutput();
+        SqlTracker tracker = new SqlTracker(connection);
+        Item replacedItem = tracker.add(new Item("Replaced item"));
+        int id = replacedItem.getId();
+        String replacedName = "New item name";
+        ReplaceAction rep = new ReplaceAction(out);
+        Input input = mock(Input.class);
+        when(input.askInt(any(String.class))).thenReturn(id);
+        when(input.askStr(any(String.class))).thenReturn(replacedName);
+        rep.execute(input, tracker);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is("=== Edit item ===" + ln + "Заявка изменена успешно." + ln));
+        assertThat(tracker.findAll().get(0).getName(), is(replacedName));
+    }
+
+    @Test
+    public void whenDeleteMockito() {
+        Output out = new StubOutput();
+        SqlTracker tracker = new SqlTracker(connection);
+        Item deleteItem = tracker.add(new Item("Bug"));
+        int id = deleteItem.getId();
+        DeleteAction del = new DeleteAction(out);
+        Input input = mock(Input.class);
+        when(input.askInt(any(String.class))).thenReturn(id);
+        del.execute(input, tracker);
+        String ln = System.lineSeparator();
+        assertThat(tracker.findById(id), is(nullValue()));
+        assertThat(out.toString(), is("=== Delete item ===="
+                + ln + "Заявка удалена успешно." + ln));
+    }
+
+    @Test
+    public void whenFindByIdMockito() {
+        Output out = new StubOutput();
+        SqlTracker tracker = new SqlTracker(connection);
+        Item findItem = tracker.add(new Item("Find me"));
+        int id = findItem.getId();
+        FindByIdAction find = new FindByIdAction(out);
+        Input input = mock(Input.class);
+        when(input.askInt(any(String.class))).thenReturn(id);
+        find.execute(input, tracker);
+        String ln = System.lineSeparator();
+        assertThat(tracker.findById(id), is(findItem));
+        assertThat(out.toString(), is("=== Find item by id ===" + ln + findItem + ln));
+    }
+
+    @Test
+    public void whenFindByNameMockito() {
+        Output out = new StubOutput();
+        SqlTracker tracker = new SqlTracker(connection);
+        Item findItem = tracker.add(new Item("Find me"));
+        String name = findItem.getName();
+        FindByNameAction find = new FindByNameAction(out);
+        Input input = mock(Input.class);
+        when(input.askStr(any(String.class))).thenReturn(name);
+        find.execute(input, tracker);
+        String ln = System.lineSeparator();
+        assertThat(tracker.findByName(name), is(List.of(findItem)));
+        assertThat(out.toString(), is("=== Find items by name ===" + ln + findItem + ln));
     }
 }
